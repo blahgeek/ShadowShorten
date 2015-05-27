7cc9fa5f5c00cbe3d3c88656a1104b793e66b722
BlahGeek <i@BlahGeek.com>
2015-05-27 11:30:35 +0000

    inject banner, add auth

--- static/shadowshorten_inject.js
+++ static/shadowshorten_inject.js
@@ -1,1 +1,5 @@
-alert('Brought to you by ShadowShorten');
+var shadowshorten_inject = '<div style="margin:0; width: 100%; position: fixed; bottom: 0; padding: 10px auto 10px; left: 0; background-color: #e0e0e0; z-index: 9999; color: black; font-size: 14px; line-height: 16px; text-align: center;">'
+                           + '<p style="font-size: 16px; ">Proxy by ShadowShorten</p>'
+                           + '<p>来自BlahGeek的科学短网址服务</p>'
+                           + '</div>';
+document.write(shadowshorten_inject);
